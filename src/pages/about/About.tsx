6c69961305fcafb5077ecda2729827ef433eafbb
Petros <petros.chantz@gmail.com>
2022-10-15 15:53:30 +0200

    feat: adds new styling to about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,19 +1,23 @@
 export const About = () => {
   return (
-    <div className="flex flex-col gap-10 px-10 py-20 my-10 lg:my-32 lg:px-32">
+    <section className="px-10 py-20 my-10 lg:my-32 xl:px-32 desktop:px-52">
       <div className="flex flex-col gap-5">
-        <h1>Hey! I'm Petros!</h1>
-        <img
-          src="https://images.pexels.com/photos/840916/pexels-photo-840916.jpeg?auto=compress&cs=tinysrgb&w=600"
-          alt=""
-          className="md:w-2/3 lg:w-2/4"
-        />
-        <div className="flex flex-col gap-5 pt-20 md:gap-10 lg:w-2/3 desktop:w-2/4">
+        <div className=" lg:self-end lg:w-2/3">
+          <h2 className="self-center lg:self-end">Hey! I'm Petros!</h2>
+        </div>
+        <div className="flex bg-center lg:self-end lg:w-2/3 ">
+          <img
+            src="https://images.pexels.com/photos/7961265/pexels-photo-7961265.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+            alt="petros-profile"
+            className="md:w-2/4 md:self-start"
+          />
+        </div>
+        <div className="flex flex-col self-end gap-5 pt-20 md:gap-10 lg:w-2/3">
           <h3>
             I'm interested in the connection between the physical and the
             digital world.
           </h3>
-          <p className="font-light text-gray-400 ">
+          <p>
             I research, explore, prototype and develop new modalities of
             interactions with code. I love translating (often) abstract and
             ambiguous user/stakeholder needs and wishes into straightforward
@@ -24,7 +28,7 @@ export const About = () => {
           <div className="flex flex-col pt-10">
             <h3 className="self-start">Currently,</h3>
             <h3 className="self-start pt-2">I work at APS Group.</h3>
-            <p className="self-start pt-10 font-light text-gray-400 ">
+            <p className="self-start pt-10">
               There, I'm leading design research initiatives and bridge the gap
               between UX/UI design and frontend development. As a UX Developer,
               I help deliver new product features to our SaaS products and make
@@ -34,6 +38,6 @@ export const About = () => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
