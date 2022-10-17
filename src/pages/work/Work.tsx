2942eee6c101f9290953c1617393f73cf6fc31ea
Petros <petros.chantz@gmail.com>
2022-10-17 15:51:08 +0200

    chore: adjusts gap between work elements

--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -2,7 +2,7 @@ import { Projects } from "./Projects";
 
 export const Work = () => {
   return (
-    <section className="flex flex-col gap-24 px-10 mt-32 lg:px-32 lg:gap-20 md:gap-32 md:mt-52">
+    <section className="flex flex-col gap-24 px-10 mt-32 lg:px-32 md:gap-32 md:my-52">
       <div className="flex flex-col self-start gap-10 ">
         <h1>Work</h1>
         <h2>A selection of different multidisciplanery projects.</h2>
