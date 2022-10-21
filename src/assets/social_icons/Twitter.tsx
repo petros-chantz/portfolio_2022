5c0f323f46b9374974a0e85a0b3212ff64724999
Petros <petros.chantz@gmail.com>
2022-10-21 21:14:48 +0200

    error fixing

--- src/assets/social_icons/Twitter.tsx
+++ src/assets/social_icons/Twitter.tsx
@@ -1,5 +1,3 @@
-import { motion } from "framer-motion";
-
 const Twitter = () => {
   return (
     <svg
--- src/pages/work/Project.tsx
+++ src/pages/work/Project.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { Exit } from "../../components/exit/Exit";
 import { Footer } from "../../components/footer/Footer";
