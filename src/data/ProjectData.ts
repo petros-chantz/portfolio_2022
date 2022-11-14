9fc9f3e7ebdc6a4e9081757f54a1c16d67a9568f
Petros <petros.chantz@gmail.com>
2022-11-14 20:02:11 +0100

    feat: adds projects

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -3,19 +3,116 @@ import { ProjectProperties } from "../interfaces/ProjectProperties.interface";
 export const ProjectData: ProjectProperties[] = [
   {
     index: 0,
-    path: "https://medium.com/@petros.chantz/aps-group-f2bcc8dbda8",
+    path: "APSGroup",
     title: "APS Group",
+    subtitle: "August 2021 - now | Work Experience",
     year: 2022,
-    thumbnail: `assets/project_photos/aps.jpg`,
+    thumbnail: `assets/project_photos/aps/aps.jpg`,
     area: "Design Research & Interaction Design",
+    keywords: [
+      "Design Research",
+      "Interaction Design",
+      "System Design",
+      "B2B Workflows",
+    ],
+    company: "APS Group, IT Development",
+    place: "Eindhoven, NL",
+    time: "16 months",
+    text: {
+      header: `APS Group is a marketing communication company with a strong presence 
+      in content creation, marketing material production and print management. I 
+      joined their IT Development team with a focus on researching and designing 
+      new product experiences for their different in-house SaaS applications.`,
+      middle: {
+        h3: `Being only the 2nd person ever to join APS as a designer, 
+        I’ve taken a leading role in expanding and implementing design 
+        research activities within the department.`,
+        p: `Before my arrival, design research within APS was rare and 
+        was heavily confined to doing small simple surveys that led to 
+        little to no actionable outcomes. To better fit the demands and 
+        needs of the department, I introduced more appropriate design 
+        research methods such as focus groups, 1-to-1 interviewing and 
+        diary studies. I also identified early on that stakeholders need 
+        help acknowledging when and how to involve design research. Thus, 
+        I created new procedures for them to request help or plan discovery sessions.`,
+      },
+      end: {
+        h3: `Almost 16 months later, I’ve conducted numerous design research studies 
+        with international clients, brands and internal stakeholders. `,
+        p: `A great highlight 
+        of my career within APS has been the development of personas and the relationships 
+        between them. By insisting on and advocating for data-driven personas to stakeholders, 
+        managers and business analysts, we have now a better and clearer view of the 
+        opportunities and challenges we’re facing.`,
+      },
+    },
+    // links: {
+    //   a: {
+    //     name: `somebkbjf`,
+    //     url: ``,
+    //   },
+    //   b: {
+    //     name: ``,
+    //     url: ``,
+    //   },
+    // },
+    photos: {
+      center: `assets/project_photos/aps/aps.jpg`,
+      section: {
+        a: `assets/project_photos/aps/teams.jpg`,
+        b: `assets/project_photos/aps/results.jpg`,
+        c: `assets/project_photos/aps/interview.jpg`,
+      },
+    },
   },
   {
     index: 1,
-    path: "https://medium.com/@petros.chantz/model-making-in-the-digital-age-da898e124b73",
+    path: "Workshop",
     title: "Model Making in the Digital Age",
+    subtitle: "August - September 2022 | Workshop",
     year: 2022,
-    thumbnail: `assets/project_photos/workshop.jpg`,
+    thumbnail: `assets/project_photos/workshop/workshop.jpg`,
     area: "Workshop",
+    keywords: [
+      "Digital Fabrication",
+      "Product Design",
+      "Model Making",
+      "Self-Development",
+    ],
+    company: "Atelier La Juntana",
+    place: "Liencres, Cantabria, Spain",
+    time: "7 days",
+    text: {
+      header: `In the summer of 2022, I participated in a 7-day intense model-making workshop 
+      organised by the Atelier La Juntana in Liencres, Cantabria, Spain.`,
+      middle: {
+        h3: `Making and designing things in the physical space is something very close to me and my design identity.`,
+        p: `I joined this workshop with the motivation of rediscovering this connection as well as learning new techniques and methods for prototyping and model-making.
+        The techniques and methods we worked on were complementary to the things I’ve known since university. I had never before worked with resin or metal casting, with photo etching or engraving. The workshop tutors always reminded us to bring back these techniques to our disciplines and helped us reflect on how after the workshop we could use them in our own design process.`,
+      },
+      end: {
+        h3: `Working alongside other designers and architects in such an unusual setting was eye-opening. `,
+        p: `The number of things I learned within that workshop as well as the shared knowledge that people passed with one another made me reflect a lot about my own skills and design processes. Finally, I believe with this experience I became more mature in my skillset, collaboration and presentation of results.`,
+      },
+    },
+    links: {
+      a: {
+        name: `Atelier La Juntana`,
+        url: `http://www.atelierlajuntana.com/`,
+      },
+      b: {
+        name: `Article I wrote`,
+        url: `https://medium.com/@petros.chantz/thoughts-on-making-a-week-in-liencres-spain-d4f625ace64e`,
+      },
+    },
+    photos: {
+      center: `assets/project_photos/workshop/together.jpg`,
+      section: {
+        a: `assets/project_photos/workshop/resin.jpg`,
+        b: `assets/project_photos/workshop/printing.jpg`,
+        c: `assets/project_photos/workshop/metal.jpg`,
+      },
+    },
   },
   // {
   //   index: 2,
@@ -27,16 +124,18 @@ export const ProjectData: ProjectProperties[] = [
   // },
   {
     index: 3,
-    path: "https://medium.com/@petros.chantz/data-driven-design-ccdae55e2edf",
+    path: "/DataDrivenDesign",
     title: "Data Driven Design",
+    subtitle: "",
     year: 2019,
     thumbnail: "assets/project_photos/driven.jpg",
     area: "Design Research",
   },
   {
     index: 4,
-    path: "https://medium.com/@petros.chantz/researching-the-future-everyday-2c840c862d8d",
+    path: "/ResearchingeEveryday",
     title: "Researching the Future Everyday",
+    subtitle: "",
     year: 2018,
     thumbnail: "assets/project_photos/future.jpg",
     area: "Design Research",
@@ -45,6 +144,7 @@ export const ProjectData: ProjectProperties[] = [
     index: 5,
     path: "https://medium.com/@petros.chantz/composing-everyday-rituals-b18a5bd3bd78",
     title: "Composing Everyday Rituals",
+    subtitle: "",
     year: 2018,
     thumbnail: "assets/project_photos/rituals.jpg",
     area: "Interaction Design",
@@ -61,6 +161,7 @@ export const ProjectData: ProjectProperties[] = [
     index: 7,
     path: "https://medium.com/@petros.chantz/researching-design-identities-f8868edde6a4",
     title: "Researching Design Identities",
+    subtitle: "",
     year: 2018,
     thumbnail: "assets/project_photos/identities.jpg",
     area: "Design Research",
@@ -69,6 +170,7 @@ export const ProjectData: ProjectProperties[] = [
     index: 8,
     path: "https://medium.com/@petros.chantz/designing-smart-iot-experiences-e6b76f420d77",
     title: "Designing Smart IoT Experiences",
+    subtitle: "",
     year: 2017,
     thumbnail: "assets/project_photos/audio.jpg",
     area: "Interaction Design",
@@ -77,6 +179,7 @@ export const ProjectData: ProjectProperties[] = [
     index: 9,
     path: "https://medium.com/@petros.chantz/researching-future-retail-experiences-cceb803e5e27",
     title: "Researching Future Retail Experiences",
+    subtitle: "",
     year: 2016,
     thumbnail: "assets/project_photos/fbp.jpg",
     area: "Design Research",
